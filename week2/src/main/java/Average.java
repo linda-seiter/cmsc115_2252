e97eb059d0c59a6e92b199043691c86d24605978
Linda Seiter <linseiter@gmail.com>
2024-10-23 08:08:05 -0400

    rename buggy classes

--- week2/src/main/java/Average.java
+++ week2/src/main/java/Average.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 /**
- * BuggyAverage reads two numbers and prints the average. There is an error in
+ * Reads two numbers and prints the average. There is an error in
  * the average calculation.
  */
-public class BuggyAverage {
+public class Average {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter 2 numbers: ");
         int num1 = input.nextInt();
         int num2 = input.nextInt();
-        double avg = num1 + num2 / 2.0;
+        double avg = num1 + num2 / 2.0; // ERROR
         System.out.println("Average = " + avg);
     }
 }
--- week2/src/main/java/ViralSpread.java
+++ week2/src/main/java/ViralSpread.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 /**
- * BuggySpread computes 3 iterations of viral spread given a particular R0.
+ * Computes 3 iterations of viral spread given a particular R0.
  * There is an error in the calculation.
  */
-public class BuggySpread {
+public class ViralSpread {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter R0: ");
         int r0 = input.nextInt();
         int iterations = 3;
-        int infected = (int) Math.pow(iterations, r0);
+        int infected = (int) Math.pow(iterations, r0); // ERROR
         System.out.println(iterations + " iterations result in " + infected + " infections");
     }
 }
